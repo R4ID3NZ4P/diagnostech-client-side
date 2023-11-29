be0d7581d462ec75aef934f211a535b78477b155
R4ID3NZ4P <iamxtremesifat@gmail.com>
2023-11-30 02:57:57 +0600

    Filter by date functionality added on All Tests page

--- src/pages/AllTests/AllTests.jsx
+++ src/pages/AllTests/AllTests.jsx
@@ -42,10 +42,39 @@ const AllTests = () => {
     }
     console.log(testData);
 
+    const handleChange = e => {
+        console.log(e.target.value);
+        const value = e.target.value;
+        const temp = testData.filter(item => {
+            return new Date(item.date) > new Date(Date.now());
+        });
+        if(value === "none") {
+            setData(temp);
+        }
+
+        else if(value === "earliest") {
+            temp.sort((a, b) => new Date(a.date) - new Date(b.date));
+            setData(temp);
+        }
+
+        else {
+            temp.sort((a, b) => new Date(b.date) - new Date(a.date));
+            setData(temp);
+        }
+    }
+
 
     return (
         <div className="min-h-screen py-12 px-5 lg:px-32 bg-gradient-to-r from-white to-[#038b9d7a] flex flex-col justify-center items-center">
             <h1 className="text-3xl font-bold text-center py-6">All Tests</h1>
+            <div className="w-full flex justify-center items-center gap-4 my-6">
+                <h3 className="text-secondary">Filter: </h3>
+                <select onChange={handleChange} className="select select-accent w-32">
+                    <option value={"none"}>None</option>
+                    <option value={"earliest"}>Earliest First</option>
+                    <option value={"latest"}>Latest First</option>
+                </select>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {data.map((test) => (
                     <TestCard key={test._id} data={test}></TestCard>
